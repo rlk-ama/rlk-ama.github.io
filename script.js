e86479597ff86f53380e2756051a33fe3ad17f59
raphael <raphael.lopezkaufman@gmail.com>
2020-04-13 18:12:55 +0200

    Gavriella Website

---
+++ script.js
@@ -0,0 +1,12 @@
+function displayMenu() {
+  var x = document.getElementById("menu-h");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  }
+} 
+function removeMenu() {
+  var x = document.getElementById("menu-h");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  }
+} 
