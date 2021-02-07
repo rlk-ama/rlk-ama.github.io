978d58f7579b254e1220bb4dfed7ff1afa1fe26e
raphael <raphael.lopezkaufman@gmail.com>
2021-02-07 20:49:51 +0100

    Remove Out of the Cube pages and point to the new website

--- script.js
+++ script.js
@@ -10,28 +10,3 @@ function removeMenu() {
     x.style.display = "none";
   }
 } 
-function submitForm() {
-  $('form').get(0).reportValidity();
-
-  var date = $('#date').val() || '';
-  var email = $('#email').val() || ''; 
-
-  var data = {
-    'entry.1496796250': date,
-    'entry.916265874': email,
-  };
-  if ($('form').get(0).checkValidity() && date && email) {
-   $.ajax({
-     type: 'POST',
-     url: 'https://docs.google.com/forms/u/2/d/e/1FAIpQLSewFMUR94aX69kxneGoqtVHX9-fFBoRv9kBKTz9M_WTEye-sw/formResponse',
-     data: data,
-     contentType: 'application/json',
-     dataType: 'jsonp',
-     complete: function() {
-       $('#submit_button').html('Thanks, we\'ll get in touch soon!');
-       $('#submit_button').css("background-color", "#00AB84");
-       $('#submit_button').prop("disabled", true);
-     }
-   });
-  }
-}
